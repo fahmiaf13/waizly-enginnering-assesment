c0c92808a208ec63088a94ff2138bf4921e76863
Fahmi Achmad <fahmiachmadf13@gmail.com>
2023-09-08 13:31:09 +0700

    fix: eslint errors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { Icon } from "@iconify/react";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="w-full bg-tomato relative z-[1]">
+      <div className="flex items-center container mx-auto ">
+        <div className="flex w-4/12 gap-3 text-secondary">
+          <a target="_blank" href="https://github.com/fahmiaf13">
+            <Icon width={36} icon="bi:github" />
+          </a>
+          <a target="_blank" href="https://www.linkedin.com/in/fahmi-achmad-fahrudin/">
+            <Icon width={36} icon="bi:linkedin" />
+          </a>
+          <a target="_blank" href="https://fahmiaf.vercel.app/">
+            <Icon width={36} icon="mdi:paper" />
+          </a>
+        </div>
+        <div className="flex py-5 justify-center flex-col w-4/12">
+          <div className="text-center  font-extrabold text-secondary">Developed by</div>
+          <div className="text-center text-sm text-secondary">Fahmi Achmad Fahrudin</div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,5 +1,5 @@
 import { Helmet } from "react-helmet-async";
-import { Navbar, Footer } from "@/components";
+
 import React from "react";
 
 interface IDefaultLayout {
